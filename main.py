12bbdc153ead9930e182e59faa8256fda5ff4fb6
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2014-02-26 08:57:45 +0000

    Step 4: Factores primos

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
+import sys
 import os
+
 from flask import Flask
 from flask import Response
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def main():
@@ -15,4 +20,17 @@ def main():
 @app.route('/ping')
 def ping():
     r = Response('{ "alive": true}', mimetype="application/json")
+    return r
+
+@app.route('/primeFactors')
+def primeFactors():
+    from primes import factors
+    number = int(request.args.get('number', 16))
+    r = Response(
+        json.dumps({
+            "number": number,
+            "decomposition" : factors(number)
+            }), 
+        mimetype="application/json"
+        )
     return r
