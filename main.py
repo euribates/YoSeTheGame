14ab38423b9e1f6657d3605e73fa2fe20ade6da0
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2014-02-27 12:39:04 +0000

    Step 4: Big number guard

--- main.py
+++ main.py
@@ -28,13 +28,23 @@ def primeFactors():
     number = request.args.get('number', 16)
     try:
         number = int(number)
-        r = Response(
-            json.dumps({
-                "number": number,
-                "decomposition" : factors(number)
-                }), 
-            mimetype="application/json"
-            )
+        if number > 1e6:
+            r = Response(
+                json.dumps({
+                    "number": number,
+                    "error" : "too big number (>1e6)",
+                    }), 
+                mimetype="application/json"
+                )
+
+        else:
+            r = Response(
+                json.dumps({
+                    "number": number,
+                    "decomposition" : factors(number)
+                    }), 
+                mimetype="application/json"
+                )
         return r
     except ValueError as err:
         r = Response(
